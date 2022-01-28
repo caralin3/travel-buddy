230c7ea071effd87456b87d4eed598c8c894a4ac
Cara Steinberg <caralin3@gmail.com>
2022-01-28 10:28:32 -0500

    Add react-router and redux

--- travel-buddy-app/src/App.tsx
+++ travel-buddy-app/src/App.tsx
@@ -1,6 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
 
 function App() {
   return (
@@ -13,6 +42,10 @@ function App() {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
       </header>
     </div>
   );
