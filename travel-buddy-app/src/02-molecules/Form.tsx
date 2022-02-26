c730f4ff203690c0bdb1b21ca82029b450787617
Cara Steinberg <caralin3@gmail.com>
2022-02-26 16:48:24 -0500

    Add form component to storybook

--- travel-buddy-app/src/02-molecules/Form.tsx
+++ travel-buddy-app/src/02-molecules/Form.tsx
@@ -3,11 +3,13 @@ import { Button, Form as BSForm, FormGroup } from 'reactstrap';
 
 export interface FormProps {
   onSubmit: () => void;
+  title: string;
 }
 
-export const Form: React.FC<FormProps> = ({ children, onSubmit }) => (
-  <BSForm onSubmit={onSubmit}>
-    <FormGroup>{children}</FormGroup>
-    <Button>Submit</Button>
+export const Form: React.FC<FormProps> = ({ children, onSubmit, title }) => (
+  <BSForm className="d-flex flex-column shadow-sm p-4 border rounded" onSubmit={onSubmit}>
+    <h2 className="mb-4">{title}</h2>
+    {children}
+    <Button className="mt-3 text-align-center align-self-center">Submit</Button>
   </BSForm>
 );
---
+++ travel-buddy-app/src/stories/02-molecules/Form.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Form } from '../../02-molecules';
+import { FormGroup, Input, Label } from 'reactstrap';
+
+export default {
+  title: 'Molecules/Form',
+  component: Form,
+  argTypes: {
+    onSubmit: { action: 'submit' },
+  },
+} as ComponentMeta<typeof Form>;
+
+const Template: ComponentStory<typeof Form> = (args) => (
+  <Form {...args}>
+    <FormGroup>
+      <Label for="exampleEmail">Email</Label>
+      <Input id="exampleEmail" name="email" placeholder="with a placeholder" type="email" />
+    </FormGroup>
+    <FormGroup>
+      <Label for="examplePassword">Password</Label>
+      <Input id="examplePassword" name="password" placeholder="password placeholder" type="password" />
+    </FormGroup>
+  </Form>
+);
+
+export const Example = Template.bind({});
+Example.args = {
+  title: 'Form Title',
+};
